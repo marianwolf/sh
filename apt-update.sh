29e9ff3e1ed8b8cb2f48e81f4eb8f2e79721cdc6
marianwolf <marian.wolf2008@gmail.com>
2026-05-20 15:54:57 +0200

    refactor(apt-update): use apt command instead of apt-get

--- apt-update.sh
+++ apt-update.sh
@@ -6,14 +6,14 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "=== Automation gestartet: $(date) ===" >> "$LOGFILE"
 
-echo "[1/5] apt-get update" >> "$LOGFILE"
-if apt-get update >> "$LOGFILE" 2>&1; then
+echo "[1/5] apt update" >> "$LOGFILE"
+if apt update >> "$LOGFILE" 2>&1; then
     echo "[2/5] apt-get dist-upgrade -y" >> "$LOGFILE"
     if apt-get dist-upgrade -y >> "$LOGFILE" 2>&1; then
-        echo "[3/5] apt-get autoclean" >> "$LOGFILE"
-        apt-get autoclean -y >> "$LOGFILE" 2>&1
-        echo "[4/5] apt-get autopurge" >> "$LOGFILE"
-        apt-get autopurge -y >> "$LOGFILE" 2>&1
+        echo "[3/5] apt autoclean" >> "$LOGFILE"
+        apt autoclean -y >> "$LOGFILE" 2>&1
+        echo "[4/5] apt autopurge" >> "$LOGFILE"
+        apt autopurge -y >> "$LOGFILE" 2>&1
         if [ -f /var/run/reboot-required ]; then
             echo "[5/5] reboot" >> "$LOGFILE"
             # Kurze Pause, damit der Log-Buffer geschrieben werden kann
